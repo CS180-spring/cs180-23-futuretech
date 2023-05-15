03afddeab60238f5ccb40ad8c107ec79451e9f1d
Colliquake <aroh002@ucr.edu>
2023-05-15 11:34:32 -0700

    added html files; added setters for singlereview

--- document/src/main/java/com/futuretech180/document/SingleReview.java
+++ document/src/main/java/com/futuretech180/document/SingleReview.java
@@ -7,6 +7,9 @@ public class SingleReview {
     private double rating;
     private String description;
 
+    public SingleReview(){
+    }
+    
     public SingleReview(String firstName, String lastName, String language,
                         String item, double rating, String description) {
         this.firstName = firstName;
@@ -16,25 +19,25 @@ public class SingleReview {
         this.rating = rating;
         this.description = description;
     }
-//    @Override
+    //    @Override
     public String toString()
     {
         return firstName + " " + time + " " + language + " "
-                + item + " " + (rating) + " " + description;
+        + item + " " + (rating) + " " + description;
     }
-
+    
     boolean keyWordSearch(String str)
     {
         return firstName.contains(str) || time.contains(str)
-                || language.contains(str) || item.contains(str)
-                || description.contains(str);
+        || language.contains(str) || item.contains(str)
+        || description.contains(str);
     }
-
+    
     boolean keyWordSearch(double expectRating)
     {
         return expectRating == rating;
     }
-
+    
     public String getFirstName() {
         return firstName;
     }
@@ -42,20 +45,45 @@ public class SingleReview {
     public String getTime() {
         return time;
     }
-
+    
     public String getLanguage() {
         return language;
     }
-
+    
     public String getItem() {
         return item;
     }
-
+    
     public double getRating() {
         return rating;
     }
-
+    
     public String getDescription() {
         return description;
     }
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+    
+    public void setTime(String time){
+        this.time = time;
+    }
+    
+    public void setLanguage(String language){
+        this.language = language;
+    }
+    
+    public void setItem(String item){
+        this.item = item;
+    }
+    
+    public void setRating(double rating){
+        this.rating = rating;
+    }
+    
+    public void setDescription(String description){
+        this.description = description;
+    }
+
 }
