f34baeea4393aab00ff364d316a4fb530f522e76
Stephen Huang <stephenhuang@Stephens-MacBook-Air.local>
2023-05-22 15:04:53 -0700

    new class to handle the path of user

---
+++ document/src/main/java/com/futuretech180/document/PathHolder.java
@@ -0,0 +1,46 @@
+package com.futuretech180.document;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class PathHolder {
+    public static void main(String[] args) throws IOException {
+        PathHolder p = new PathHolder("user1");
+        System.out.println(p.isFolderExisted("user1"));
+    }
+    String username;
+    PathHolder(String folderName) {
+        username = folderName;
+    }
+
+    public void createUserFolder(String folderName) throws IOException {
+        username = folderName;
+        if(!isFolderExisted(folderName))
+        {
+            Path path = Paths.get(getPath());
+            Files.createDirectories(path);
+        }
+    }
+
+    public String getPath()
+    {
+        return "/Users/stephenhuang/IdeaProjects/cs180-23-futuretech/" +
+                "Users/"+ username + "/";
+
+    }
+
+    public boolean isFolderExisted(String folderName) throws IOException {
+        username = folderName;
+        System.out.println(getPath());
+        File file = new File(getPath());
+        return file.isDirectory();
+    }
+
+    public boolean isFileExisted() throws IOException {
+        Path tempDirectory = Files.createTempDirectory(getPath());
+        return Files.exists(tempDirectory);
+    }
+}
