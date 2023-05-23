0937daddc9de59b45a0884bd7cb83e050828b1c8
yongkang guan <yguan039@ucr.edu>
2023-05-23 01:35:07 -0700

    update comments

--- document/src/main/java/com/futuretech180/document/DeleteData.java
+++ document/src/main/java/com/futuretech180/document/DeleteData.java
@@ -18,6 +18,11 @@ public class DeleteData {
     public static void main(String[] args) throws IOException, ParseException {
         Scanner scanner = new Scanner(System.in);
 
+        // JSON Data Reading:
+        // The existing JSON data is read from a file named "output.json" and stored in the jsonData string.
+        // If the file is not empty, its content is read using Files.readAllBytes() and converted to a string.
+        // If there are any errors during file reading, an exception is caught and printed.
+
         // Read the existing JSON data from the file
         String jsonData = "";
         File file = new File("output.json");
@@ -29,8 +34,11 @@ public class DeleteData {
             }
         }
 
-        // If document empty, create a new object and push into the array
-         
+        // JSON Parsing:
+        // If the jsonData string is not empty, it is parsed using JSONParser from the net.minidev.json.parser package.
+        // If the parsing is successful, the parsed object is cast to a JSONObject.
+        // If the jsonData string is empty, a new JSONObject is created and an empty JSONArray is added under the key "items_list".
+
         JSONObject obj;
         if (!jsonData.isEmpty()) {
             obj = (JSONObject) new JSONParser(JSONParser.MODE_PERMISSIVE).parse(jsonData);
@@ -38,6 +46,13 @@ public class DeleteData {
             obj = new JSONObject();
             obj.put("items_list", new JSONArray());
         }
+
+        // Item Deletion:
+        // The user is prompted to enter the name of the item to delete.
+        // The user's input is read using the Scanner object.
+        // A loop iterates over the existingList JSONArray to search for an item with a matching name.
+        // If a match is found, the item is removed from the existingList.
+        // A boolean flag itemFound is used to track whether the item was found or not.
 
         JSONArray existingList = (JSONArray) obj.get("items_list");
 
@@ -56,10 +71,21 @@ public class DeleteData {
             }
         }
 
+        // Item Not Found Handling:
+        // If the itemFound flag is false, i.e., the item was not found, a message is printed indicating that the item was not found.
+        
         // Output a message if the item was not found
         if (!itemFound) {
             System.out.println("Item not found.");
         }
+
+        // JSON Data Updating and Writing:
+        // The updated JSON data is written back to the file using a FileWriter.
+        // A new JSONArray named updatedList is created to store the updated items.
+        // The items from the existingList are iterated, and each item's properties are extracted and added to a new LinkedHashMap object.
+        // The new object is then added to the updatedList.
+        // The obj is updated with the updatedList and written to the file using fileWriter.write().
+        // If there are any errors during file writing, an exception is caught and printed.
 
         // Write the updated JSON data back to the file
         try (FileWriter fileWriter = new FileWriter(file)) {
--- document/src/main/java/com/futuretech180/document/DeleteLine.java
+++ document/src/main/java/com/futuretech180/document/DeleteLine.java
@@ -4,15 +4,33 @@ import java.util.Scanner;
 
 public class DeleteLine {
     public static void main(String[] args) {
+
+        // User Input:
+        // The user is prompted to enter the line number to be deleted.
+        // The input is read using a Scanner object, and the line number is stored in the lineNumber variable.
+        // The Scanner object is then closed to release system resources.
+
         // Prompt the user to enter the line number to be deleted
         System.out.print("Enter the line number to be deleted: ");
         Scanner scanner = new Scanner(System.in);
         int lineNumber = scanner.nextInt();
         scanner.close();
 
+        // File Initialization:
+        // Two File objects, inputFile and tempFile, are created to represent the input file and a temporary file, respectively. 
+        // The input file is named "data.txt", and the temporary file is named "temp.txt".
+
         // The file to be modified
         File inputFile = new File("data.txt");
         File tempFile = new File("temp.txt");
+
+        // File Modification:
+        // The code is wrapped in a try-catch block to handle any potential IOException.
+        // A BufferedReader object is created to read the contents of the input file using a FileReader.
+        // A BufferedWriter object is created to write to the temporary file using a FileWriter.
+        // A loop is used to read each line from the input file and write it to the temporary file, excluding the line specified by the lineNumber.
+        // The loop maintains a currentLineNumber counter to compare against the lineNumber input.
+        // After the loop finishes, both the reader and writer objects are closed.
 
         try {
             // Create a BufferedReader object to read the input file
@@ -36,6 +54,11 @@ public class DeleteLine {
             reader.close();
             writer.close();
 
+            // File Replacement:
+            // The original input file is deleted using the delete() method of the File class.
+            // The temporary file is renamed to the original file name using the renameTo() method.
+            // The output is printed, confirming the deletion of the specified line.
+
             // Delete the original file
             inputFile.delete();
 
@@ -44,7 +67,11 @@ public class DeleteLine {
 
             System.out.println("Line " + lineNumber + " has been deleted from the file.");
 
-        } catch (IOException e) {
+        } 
+
+        // Exception Handling:
+        // If an IOException occurs during file operations, the stack trace is printed.
+        catch (IOException e) {
             e.printStackTrace();
         }
     }
