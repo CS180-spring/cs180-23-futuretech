544577dd14ece766e00f0c6f998c7888ea6d4162
yongkang guan <yguan039@ucr.edu>
2023-05-25 00:59:23 -0700

    update files

--- document/src/main/java/com/futuretech180/document/GreetingController.java
+++ document/src/main/java/com/futuretech180/document/GreetingController.java
@@ -7,8 +7,18 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+// @Controller Annotation:
+// The @Controller annotation is used to mark the class as a Spring MVC controller.
+
 @Controller
 public class GreetingController {
+
+    // Request Mapping Methods:
+    // @GetMapping("/greeting") is a handler method that handles GET requests to "/greeting".
+    // It accepts an optional request parameter named "name" and a Model object.
+    // The method adds the "name" attribute to the model and returns the view name "greeting".
+    // The value of the "name" attribute is either the value of the "name" request parameter or the default value "World".
+    
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
@@ -29,16 +39,27 @@ public class GreetingController {
     //     return "register_success";
     // }
 
+    // Model Attributes:
+    // @ModelAttribute("account") is a method that provides a model attribute named "account".
+    // The method returns a new instance of the UserAccountImpl class.
 
     @ModelAttribute("account")
     public UserAccountImpl getUserAccount(){
         return new UserAccountImpl();
     }
     
+    // Request Mapping Methods for Login and Registration:
+    //@GetMapping("/login") is a handler method that handles GET requests to "/login".
+    //It returns the view name "login_form".
+
     @GetMapping("/login")
     public String showLogin(){
         return "login_form";
     }
+
+    // @PostMapping("login") is a handler method that handles POST requests to "/login".
+    // It accepts a UserAccountImpl object annotated with @ModelAttribute and performs login validation.
+    // If the login is successful, it returns the view name "login_success"; otherwise, it returns "login_failure".
 
     @PostMapping("login")
     public String handle_login(@ModelAttribute("accountDisplay") UserAccountImpl userAcc){
@@ -52,10 +73,17 @@ public class GreetingController {
         }
     }
 
+    // @GetMapping("/register2") is a handler method that handles GET requests to "/register2".
+    // It returns the view name "register_form2".
+
     @GetMapping("/register2")
     public String showForm2(){
         return "register_form2";
     }
+
+    // @PostMapping("/register2") is a handler method that handles POST requests to "/register2".
+    // It accepts a UserAccountImpl object annotated with @ModelAttribute and performs registration validation.
+    // If the registration is successful, it returns the view name "register_success"; otherwise, it returns "dupe_username".
 
     @PostMapping("/register2")
     public String handle_form2(@ModelAttribute("accountDisplay") UserAccountImpl userAcc){
--- document/src/main/java/com/futuretech180/document/MainMenuController.java
+++ document/src/main/java/com/futuretech180/document/MainMenuController.java
@@ -19,24 +19,43 @@ import net.minidev.json.JSONObject;
 import net.minidev.json.parser.JSONParser;
 import net.minidev.json.parser.ParseException;
 
+// @Controller Annotation:
+// The @Controller annotation is used to mark the class as a Spring MVC controller.
+
 @Controller
 public class MainMenuController {
+
+    // Request Mapping Method for Main Menu:
+    // @GetMapping("/mainmenu") is a handler method that handles GET requests to "/mainmenu".
+    // It returns the view name "login_success".
+
     @GetMapping("/mainmenu")
     public String retMainMenu(){
         return "login_success";
     }
 
-
+    // Model Attributes:
+    // @ModelAttribute("singleReview") is a method that provides a model attribute named "singleReview".
+    // The method returns a new instance of the SingleReview class.
 
     @ModelAttribute("singleReview")
     public SingleReview getSingleReview(){
         return new SingleReview();
     }
 
+    // Request Mapping Methods for Inserting and Viewing Data:
+    // @GetMapping("/insert") is a handler method that handles GET requests to "/insert".
+    // It returns the view name "insert_data".
+
     @GetMapping("/insert")
     public String showInsert(){
         return "insert_data";
     }
+
+    // @PostMapping("/insert") is a handler method that handles POST requests to "/insert".
+    // It accepts a SingleReview object annotated with @ModelAttribute and handles the insertion of the review data.
+    // The method calls the AddDataHelper.addItem(review) method to add the review data.
+    // It returns the view name "insert_success".
 
     @PostMapping("/insert")
     public String handleInsert(@ModelAttribute("review") SingleReview review) throws ParseException{
@@ -44,7 +63,8 @@ public class MainMenuController {
         return "insert_success";
     }
 
-
+    // @GetMapping("/view") is a handler method that handles GET requests to "/view".
+    // It returns the view name "view_data".
     
     @GetMapping("/view")
     public String showView(){
