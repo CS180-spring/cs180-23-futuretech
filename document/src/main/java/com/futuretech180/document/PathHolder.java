774addbbaf6f5f63cdd894455bb3ea5f9ea1e220
Stephen Huang <stephenhuang@Stephens-Air.lan>
2023-05-31 17:21:47 -0700

    update pathholder

--- document/src/main/java/com/futuretech180/document/PathHolder.java
+++ document/src/main/java/com/futuretech180/document/PathHolder.java
@@ -7,28 +7,9 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class PathHolder {
-    // The main entry point of the program
-    public static void main(String[] args) throws IOException {
-        // Create an instance of PathHolder with username "user1"
-        PathHolder p = new PathHolder("user1");
-
-        // Check if the folder "user1" exists and print the result
-        System.out.println("Folder 'user1' exists: " + p.isFolderExisted("user1"));
-
-        // Check if the file "sample.json" exists and print the result
-        System.out.println("File 'sample.json' exists: " + p.isFileExisted("sample.json"));
-
-        // Check if the file "sample1.json" exists and print the result
-        System.out.println("File 'sample1.json' exists: " + p.isFileExisted("sample1.json"));
-
-        // Create a folder named "user1" if it doesn't exist
-        p.createUserFolder("user1");
-    }
-
     // Variable to store the username (folder name)
     String username;
 
-    // Constructor for the PathHolder class
     PathHolder(String folderName) {
         username = folderName;
     }
@@ -40,23 +21,12 @@ public class PathHolder {
 
         String pathToU = "Users/" + folderName;
         new File(pathToU).mkdirs();
-
-        // Check if the folder already exists
-//        if (!isFolderExisted(folderName)) {
-//            // Create the folder if it doesn't exist
-//            Path path = Paths.get(getPath());
-//            Files.createDirectories(path);
-//            System.out.println("User folder '" + folderName + "' created.");
-//        } else {
-//            System.out.println("User folder '" + folderName + "' already exists.");
-//        }
     }
 
     // Get the path of the user folder
     public String getPath() {
         // Construct the path by combining the base directory with the username (folder name)
-        return "/Users/stephenhuang/IdeaProjects/cs180-23-futuretech" +
-                "/Users/" + username + "/";
+        return "/Users/" + username + "/";
     }
 
     // Check if the user folder exists
@@ -73,13 +43,18 @@ public class PathHolder {
 
     // Check if a file exists in the user folder
     public boolean isFileExisted(String fileName) throws IOException {
-        // Create a File object representing the specified file within the user folder
+
         File file = new File(getPath() + fileName);
 
-        // Check if the file exists
         return file.exists();
     }
-}
 
-// this code provides a basic framework for managing user folders and checking for folder and file existence. 
-// However, it has a hardcoded base directory path and may not work as expected in different environments or operating systems.
+    public static void main(String[] args) throws IOException {
+        PathHolder p = new PathHolder("user1");
+
+        System.out.println("Folder 'user1' exists: " + p.isFolderExisted("user1"));
+        System.out.println("File 'sample.json' exists: " + p.isFileExisted("sample.json"));
+        System.out.println("File 'sample1.json' exists: " + p.isFileExisted("sample1.json"));
+        p.createUserFolder("user1");
+    }
+}
